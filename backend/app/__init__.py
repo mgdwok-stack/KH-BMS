ccdf80a1025cce393e1cc152a7a353843f909134
mgdwok-stack <mgdwok@gmail.com>
2026-03-24 11:11:33 +0900

    Delete backend directory

--- backend/app/__init__.py
+++
@@ -1,4 +0,0 @@
-"""
-Backend Application Package
-"""
-__version__ = "1.0.0"
--- backend/app/tasks/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Tasks Package - Celery 백그라운드 작업
-"""
-from .scheduler import celery_app
-
-__all__ = ["celery_app"]
